31489a85a43a79454b84a27b39c95413b169ac28
PaulAroo <timmypaul2006@gmail.com>
2024-03-15 12:26:52 +0100

    style: root layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,14 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router"
 
+import brandlogo from "/logo.svg"
+
 export const Route = createRootRoute({
-	component: () => <Outlet />,
+	component: () => (
+		<div className="container min-h-dvh pt-[11.25rem]">
+			<div className="w-fit m-auto">
+				<img src={brandlogo} alt="quencode logo" />
+			</div>
+			<Outlet />
+		</div>
+	),
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+			padding: "1.5rem",
+			screens: {
+				"2xl": "1512px",
+			},
+		},
+		extend: {
+			colors: {
+				brand: {
+					DEFAULT: "#316FEA",
+					gray: {
+						DEFAULT: "#D3D8DC",
+						800: "#060E1E",
+						900: "#1A1919",
+					},
+				},
+			},
+		},
 	},
 	plugins: [],
 }
